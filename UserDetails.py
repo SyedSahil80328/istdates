8ee7ff85df1491f8fc618dcef5531674ef1b2e09
Syed Sahil <120191200+SyedSahil80328@users.noreply.github.com>
2024-05-18 12:52:50 +0530

    Enhanced Credentials Database

--- UserDetails.py
+++ UserDetails.py
@@ -1,5 +1,5 @@
 import sqlite3
-import TDES
+import pandas as pd
 connect = sqlite3.connect('users.db')
 cursor = connect.cursor()
 
@@ -11,21 +11,14 @@ try:
     cursor.execute("""CREATE TABLE IF NOT EXISTS BOOKING_PHASE2 (PASSENGER_ID CHAR(20) PRIMARY KEY, BOOKING_ID CHAR(20) NOT NULL,
     NAME VARCHAR(50) NOT NULL, AGE INT NOT NULL, GENDER VARCHAR(6) NOT NULL, 
     BERTH VARCHAR(20) NOT NULL, RATE NUMERIC(9,3) NOT NULL);""")
-    print("Creation successful.")
-    cursor.execute("""SELECT * FROM BOOKING_PHASE1;""")
-    e = cursor.fetchall()
-    for i in e:
-        print(i)
-    cursor.execute("""SELECT * FROM BOOKING_PHASE2;""")
-    f = cursor.fetchall()
-    for i in f:
-        print(i)
+
     cursor.execute("""SELECT * FROM USERS;""")
     f = cursor.fetchall()
-    for i in f:
-        print(i)
+    userData = pd.DataFrame(f, columns=["User ID", "Name", "Email", "Password", "Category"])
+    with pd.option_context('display.max_colwidth', None):
+        print(userData.map(repr))
+
 finally:
     cursor.close()
     connect.commit()
     connect.close()
-    print("Database closed successfully.")
